0d352ec453e54fd38c001ac0d52f7e0983c4a1b7
jimmy <jimmy.nguyen@atlanticbt.com>
2015-09-21 08:31:47 -0400

    revealing modular pattern

--- Mavenlink.user.js
+++ Mavenlink.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Mavenlink
-// @version      0.4.7
+// @version      0.4.8
 // @updateURL    https://github.com/neilgaietto/UserScripts/raw/master/Mavenlink.user.js
 // @description  ABT Mavenlink Script
 // @author       ABT
@@ -8,113 +8,126 @@
 // @grant        none
 // ==/UserScript==
 
-// functions
-var getQueryParameters = function (str) {
-    return (str || document.location.search).replace(/(^\?)/, '').split("&").map(function (n) { return n = n.split("="), this[n[0]] = n[1], this }.bind({}))[0];
-};
+window.abtMaven = (function() {
 
-var initYourTasksView = function () {
-    // remove pto tasks
-    var ptoTasks = $('tr[data-story-id=96718717], tr[data-story-id=96718727]');
-    ptoTasks.remove();
+    //========== Utilities ==========//
+    function getQueryParameters(str) {
+        return (str || document.location.search).replace(/(^\?)/, '').split("&").map(function(n) {
+            return n = n.split("="), this[n[0]] = n[1], this
+        }.bind({}))[0];
+    };
 
-    // remove milestones
-    $('tr.milestone').remove();
-
-    // hightlight tasks
-    $('.task.substory').css('background-color', 'lightcyan');
-
-    // add Type
-    if ($('.task-tracker table thead tr:contains("Type")').length === 0) {
-        $('.task-tracker table thead tr').append("<th class=\"assignees\" data-column=\"6\" tabindex=\"0\" scope=\"col\" role=\"columnheader\" aria-disabled=\"false\" unselectable=\"on\" aria-sort=\"none\" style=\"-webkit-user-select: none;\"><div class=\"tablesorter-header-inner\">Type</div></th>")
-
-        $('.task-tracker table tbody').find('tr').each(function () {
-            var classNames = $(this).attr('class').split(" ");
-            var taskType = classNames[0];
-
-            $(this).append("<td class=\"due-date\">" + taskType + "</td>");
-        })
+    function expandableDescriptionTextbox() {
+        $('.task-tracker .detail-layout .view .story-details .left .section.description .story-description textarea').css('resize', 'both');
     }
-};
 
-var updateYourTasksView = function() {
-    $('.task-tracker table tbody').find('tr').each(function() {
-        if($(this).find('td').length === 6) {
-            var classNames = $(this).attr('class').split(" ");
-            var taskType = classNames[0];
+    //========== Functions ==========//
+    function initYourTasksView() {
+        // remove pto tasks
+        var ptoTasks = $('tr[data-story-id=96718717], tr[data-story-id=96718727]');
+        ptoTasks.remove();
 
-            $(this).append("<td class=\"due-date\">" + taskType + "</td>");
+        // remove milestones
+        $('tr.milestone').remove();
+
+        // hightlight tasks
+        $('.task.substory').css('background-color', 'lightcyan');
+
+        // add Type
+        if ($('.task-tracker table thead tr:contains("Type")').length === 0) {
+            $('.task-tracker table thead tr').append("<th class=\"assignees\" data-column=\"6\" tabindex=\"0\" scope=\"col\" role=\"columnheader\" aria-disabled=\"false\" unselectable=\"on\" aria-sort=\"none\" style=\"-webkit-user-select: none;\"><div class=\"tablesorter-header-inner\">Type</div></th>")
+
+            $('.task-tracker table tbody').find('tr').each(function() {
+                var classNames = $(this).attr('class').split(" ");
+                var taskType = classNames[0];
+
+                $(this).append("<td class=\"due-date\">" + taskType + "</td>");
+            })
         }
-    });
-};
+    };
 
-var updateTasksMenuItem = function() {
-	var lnk = $('.navigation-application .link.tasks.application-link a.outer-link');
-	lnk.attr('href','/stories#upcoming?assignedToYou=true');
-	lnk.find('span.nav-link-text').text('Your Tasks');
-};
+    function updateYourTasksView() {
+        $('.task-tracker table tbody').find('tr').each(function() {
+            if ($(this).find('td').length === 6) {
+                var classNames = $(this).attr('class').split(" ");
+                var taskType = classNames[0];
 
-var expandableDescriptionTextbox = function() {
- $('.task-tracker .detail-layout .view .story-details .left .section.description .story-description textarea').css('resize','both');
-}
-
-var init = function () {
-    // reset vars
-    params = getQueryParameters(window.location.search);
-
-    // Tasks > Your Tasks
-    if (window.location.href === "https://atlanticbt.mavenlink.com/stories#upcoming?assignedToYou=true") {
-        initYourTasksView();
-
-        // add table sort, http://mottie.github.io/tablesorter/docs/index.html
-        if (!loaded) {
-            $.getScript("https://mottie.github.io/tablesorter/dist/js/jquery.tablesorter.min.js", function (data, textStatus, jqxhr) {
-                $('.task-tracker table').tablesorter({
-                    sortList: [[0, 0], [6, 0]]
-                });
-            });
-            loaded = true;
-        }
-
-        $(document).ajaxComplete(function (event, xhr, settings) {
-            updateYourTasksView();
+                $(this).append("<td class=\"due-date\">" + taskType + "</td>");
+            }
         });
-    }
+    };
 
-    // Project > Task Tracker
-    if (params.tab === "local-tracker") {
-        if(!loaded) {
-            // expand all
-            $(document).ajaxComplete(function (event, xhr, settings) {
-                // milestone clicked
-                $('.row-wrapper > .rows > .row').on('click', function () {
-                    milestoneIndex = $(this).index();
-                    milestone = $('.row-wrapper > .rows > .row')[milestoneIndex];
-                })
+    function updateTasksMenuItem() {
+        var lnk = $('.navigation-application .link.tasks.application-link a.outer-link');
+        lnk.attr('href', '/stories#upcoming?assignedToYou=true');
+        lnk.find('span.nav-link-text').text('Your Tasks');
+    };
 
-                $(milestone).children('ul.substory-rows').children('li:has(div[data-status][data-status!=completed][data-status!=resolved])').find('.toggler.substory-toggle:not(.expanded):not(.working):not(.invisible)').each(function () {
-                    $(this).addClass('working').click();
+    function init() {
+        // Update Tasks link to My Tasks
+        updateTasksMenuItem();
+
+        // Make description textboxes expandable
+        expandableDescriptionTextbox();
+
+        // reset vars
+        params = getQueryParameters(window.location.search);
+
+        // Tasks > Your Tasks
+        if (window.location.href === "https://atlanticbt.mavenlink.com/stories#upcoming?assignedToYou=true") {
+            initYourTasksView();
+
+            // add table sort, http://mottie.github.io/tablesorter/docs/index.html
+            if (!loaded) {
+                $.getScript("https://mottie.github.io/tablesorter/dist/js/jquery.tablesorter.min.js", function(data, textStatus, jqxhr) {
+                    $('.task-tracker table').tablesorter({
+                        sortList: [
+                            [0, 0],
+                            [6, 0]
+                        ]
+                    });
                 });
+                loaded = true;
+            }
+
+            $(document).ajaxComplete(function(event, xhr, settings) {
+                updateYourTasksView();
             });
-
-            loaded = true;
         }
-    }
 
-	//Update Tasks link to My Tasks
-	updateTasksMenuItem();
+        // Project > Task Tracker
+        if (params.tab === "local-tracker") {
+            if (!loaded) {
+                // expand all
+                $(document).ajaxComplete(function(event, xhr, settings) {
+                    // milestone clicked
+                    $('.row-wrapper > .rows > .row').on('click', function() {
+                        milestoneIndex = $(this).index();
+                        milestone = $('.row-wrapper > .rows > .row')[milestoneIndex];
+                    })
 
-	//Make description textboxes expandable
-	expandableDescriptionTextbox();
-};
+                    $(milestone).children('ul.substory-rows').children('li:has(div[data-status][data-status!=completed][data-status!=resolved])').find('.toggler.substory-toggle:not(.expanded):not(.working):not(.invisible)').each(function() {
+                        $(this).addClass('working').click();
+                    });
+                });
 
-// vars
-var loaded = false;
-var milestone;
-var milestoneIndex;
-var params = getQueryParameters(window.location.search);
+                loaded = true;
+            }
+        }
+    };
+
+    // vars
+    var loaded = false;
+    var milestone;
+    var milestoneIndex;
+    var params = getQueryParameters(window.location.search);
+
+    return {
+        init: init
+    };
+})();
 
 // ajax events
-$(document).ajaxComplete(function (event, xhr, settings) {
-    init();
+$(document).ajaxComplete(function(event, xhr, settings) {
+    abtMaven.init();
 });
